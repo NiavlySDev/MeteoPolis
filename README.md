f50459de1049f526337262f98b28ef27b7bf787b
NiavlyS Dev <niavlys.dev@gmail.com>
2024-04-06 17:34:57 +0200

    v0.1
    Base

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Projet3
+Projet n°3
+- Sylvain Crocquevieille
+- Victor Gauvrit
+- Maxence Antonio
+
+Maîtres Batisseurs
---
+++ SimCity/api.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+def centrer_fenetre(fenetre, largeur, hauteur):
+    largeur_ecran = fenetre.winfo_screenwidth()
+    hauteur_ecran = fenetre.winfo_screenheight()
+    x = (largeur_ecran - largeur) // 2
+    y = (hauteur_ecran - hauteur) // 2
+    fenetre.geometry(f"{largeur}x{hauteur}+{x}+{y}")
+
+def creer_boutons(fenetre, carte):
+    x2=33
+    for ligne in carte:
+        y2=33
+        for case in ligne:
+            if case==1:
+                image_originale = Image.open("ressources/map/nature.png")
+            if case==2:
+                image_originale = Image.open("ressources/map/ville.png")
+            if case==3:
+                image_originale = Image.open("ressources/map/emploie.png")
+            if case==4:
+                image_originale = Image.open("ressources/map/energie.png")
+            if case==5:
+                image_originale = Image.open("ressources/map/detruit.png")
+            image_redimensionnee = image_originale.resize((30, 30))
+            image_tk = ImageTk.PhotoImage(image_redimensionnee)
+            bouton=tk.Button(fenetre, image=image_tk)
+            bouton.image = image_tk
+            bouton.pack()
+            bouton.place(x=x2,y=y2)
+            y2+=33
+        x2+=33
+
+def creer_texte(fenetre, x2, y2, texte):
+    texte_label = tk.Label(fenetre, text=texte)
+    texte_label.place(x=x2, y=y2)
